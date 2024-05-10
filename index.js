9685ca060473c87adbd663df31455456a47d4c16
Jetezh 123 <vegasetiawan16@gmail.com>
2024-05-10 07:49:11 +0000

    add solution

--- index.js
+++ index.js
@@ -20,8 +20,16 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ip = req.ip;
+  const lang = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+
+  res.json({ 
+    ipaddress: ip,
+    language: lang,
+    software: software
+  });
 });
 
 // listen for requests :)
